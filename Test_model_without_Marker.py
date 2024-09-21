1f2d97bed5216c766f246b676a3025f030a8cf39
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-09-21 17:15:33 +0530

    Model test file to test the model without marker

---
+++ Test_model_without_Marker.py
@@ -0,0 +1,132 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import mediapipe as mp
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import LSTM,Dense # type: ignore
+from tensorflow.keras.callbacks import TensorBoard # type: ignore
+
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468 * 3)
+    left_hand = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
+    right_hand = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21 * 3)
+    
+    return np.concatenate([pose, face, left_hand, right_hand])
+
+# Path for exported data, numpy arrays
+DATA_PATH = os.path.join('Dataset')
+
+# Actions that we try to detect
+actions = np.array(['hello'])
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequences_length = 30
+
+# Creating a folder for each action like : hello, thank you, good, afternoon
+
+for action in actions: 
+    for sequence in range(no_sequences):
+        try: 
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
+        
+label_map = {label:num for num, label in enumerate(actions)}
+print(label_map)
+
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequences_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+        
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir) 
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='tanh', input_shape=(30, 1662))) # Match input shape
+model.add(LSTM(128, return_sequences=True, activation='tanh'))
+model.add(LSTM(64, return_sequences=False, activation='tanh'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))  # Number of actions
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.load_weights('model.h5')
+
+# Detection variables
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.8    #! Threshold for prediction probability (How much model is sure about the prediction)
+
+cap = cv2.VideoCapture(0)
+# Set media pipe model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        
+        # Read feed
+        ret, frame = cap.read()
+        
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        
+        # Draw landmarks
+        # draw_landmarks(image, results)
+        # draw_styled_landmarks(image, results)
+        
+        # Prediction logic
+        keypoints = extract_keypoints(results)
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+        
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
+            predictions.append(np.argmax(res))
+            
+        # Screen logic
+            if np.unique(predictions[-10:])[0] == np.argmax(res): 
+                if res[np.argmax(res)] > threshold: 
+                    if len(sentence) > 0: 
+                        if actions[np.argmax(res)] != sentence[-1]:
+                            sentence.append(actions[np.argmax(res)])
+                    else:
+                        sentence.append(actions[np.argmax(res)])
+
+            if len(sentence) > 5: 
+                sentence = sentence[-5:]
+            
+            cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+            cv2.putText(image, ' '.join(sentence), (3,30), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)        
+        
+        # Show to screen
+        cv2.imshow('Sign language detection', image)
+        
+        # Terminate program
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
